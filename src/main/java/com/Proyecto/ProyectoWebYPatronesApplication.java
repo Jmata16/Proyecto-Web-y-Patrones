f0fe13a8ab219ad4fed62baedf972887631e2825
Jose Daniel Mata <105888177+Jmata16@users.noreply.github.com>
2023-03-12 19:54:13 -0600

    Primer commit

---
+++ src/main/java/com/Proyecto/ProyectoWebYPatronesApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoWebYPatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoWebYPatronesApplication.class, args);
+	}
+
+}
