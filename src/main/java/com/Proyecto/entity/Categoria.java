7e7ffc66a1684a3f3fa9c3968c51b6bf6fd70b31
pabloguerrero16 <pabloggz31@gmail.com>
2023-04-13 12:58:01 -0600

    Error en Categorías solucionado

--- src/main/java/com/Proyecto/entity/Categoria.java
+++ src/main/java/com/Proyecto/entity/Categoria.java
@@ -17,14 +17,14 @@ import lombok.Data;
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private long id;
     private String nombre;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
